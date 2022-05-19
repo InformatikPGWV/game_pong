37af80beb683823c68e08d7b0ad61aa8f189c933
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2022-05-19 18:30:04 +0200

    first working version with WebSockets

--- wsHandler.py
+++ wsHandler.py
@@ -5,6 +5,7 @@ try:
     from websockets import connect
     import asyncio
     import json
+    import threading
 except:
     import subprocess
     subprocess.call("pip install -r requirements.txt", shell=True)
@@ -12,9 +13,14 @@ except:
     from websockets import connect
     import asyncio
     import json
+    import threading
 
+# from main import player1
+# from main import player2
 
 # Code
+
+
 def runWsReciever():
     asyncio.run(recieveWsData("wss://wss.astrago.de"))
 
@@ -26,12 +32,30 @@ async def recieveWsData(uri):
 
 
 def handleWsEvent(event):
+    print(event)
     recievedWsEvent = json.loads(event)
+
     # Pong Game
     if recievedWsEvent["game"] == "pong":
+
         # Actions of 1st Player
         if recievedWsEvent["sender"] == "player1":
-            if recievedWsEvent["data"]["action"] == "upPredded":
-                pass
-            elif recievedWsEvent["data"]["action"] == "upReleased":
-                pass
+
+            # # Up Key
+            # if recievedWsEvent["data"]["action"] == "upPredded":
+            #     main.player1.UP_KEY = True
+            # elif recievedWsEvent["data"]["action"] == "upReleased":
+            #     main.player1.UP_KEY = False
+
+            # # Down Key
+            # if recievedWsEvent["data"]["action"] == "downPredded":
+            #     main.player1.DOWN_KEY = True
+            # elif recievedWsEvent["data"]["action"] == "downReleased":
+            #     main.player1.DOWN_KEY = False
+
+            pass
+
+
+wsResciever = threading.Thread(target=runWsReciever)
+wsResciever.start()
+wsResciever.join()
